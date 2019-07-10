dab00eb17022165f53d0745d3a4a0a98a8018649
骑猪o0o找牛 <wyj317005934@163.com>
2019-07-10 13:34:24 +0800

    阿道夫

---
+++ KnobControl/KnobControl.h
@@ -0,0 +1,19 @@
+//
+//  KnobControl.h
+//  KnobControl
+//
+//  Created by das on 2019/7/10.
+//  Copyright © 2019 das. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KnobControl.
+FOUNDATION_EXPORT double KnobControlVersionNumber;
+
+//! Project version string for KnobControl.
+FOUNDATION_EXPORT const unsigned char KnobControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KnobControl/PublicHeader.h>
+
+
